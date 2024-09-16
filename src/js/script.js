89b908dba556567f514e472cbc34f7dc5680a62a
gerardo.mosca <gerimosca90@gmail.com>
2024-09-16 16:33:40 +0200

    actualizado js

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,48 @@
 const formElement = document.getElementById('form');
+const tasksElement = document.getElementById('tasks');
 
 const allTasks = [
   {
     id: Date.now(),
-    taskName: taskName,
+    taskName: 'Terminar de programar el To-Do List',
     completed: false,
   },
 ];
 
+const printTasks = () => {
+  const fragment = document.createDocumentFragment();
+
+  allTasks.forEach((task) => {
+    const newTaskContainer = document.createElement('div');
+    newTaskContainer.classList.add('task-container');
+
+    const newTaskCheck = document.createElement('input');
+    newTaskCheck.type = 'checkbox';
+    newTaskCheck.classList.add('task-check');
+    newTaskCheck.checked = task.completed;
+    newTaskCheck.id = task.id;
+    newTaskCheck.dataset.id = task.id;
+
+    const newTaskLabel = document.createElement('label');
+    newTaskLabel.classList.add('task-text');
+    newTaskLabel.textContent = task.taskName;
+    newTaskLabel.htmlFor = task.id;
+
+    const newTaskDelete = document.createElement('img');
+    newTaskDelete.classList.add('task-delete');
+    newTaskDelete.src = './docs/assets/images/icon-cross.svg';
+
+    newTaskContainer.append(newTaskCheck, newTaskLabel, newTaskDelete);
+    fragment.append(newTaskContainer);
+  });
+
+  tasksElement.textContent = '';
+  tasksElement.append(fragment);
+};
+
 const saveTask = (task) => {
   allTasks.push(task);
-  console.log(allTasks);
+  printTasks();
 };
 
 const createTask = (taskName) => {
@@ -27,4 +59,5 @@ formElement.addEventListener('submit', (event) => {
   event.preventDefault();
   if (!event.target.task.value.trim()) return;
   createTask(event.target.task.value);
+  event.target.reset();
 });
