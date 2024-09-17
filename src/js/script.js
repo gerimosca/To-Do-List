f163b5233483b0f5589b17c23152694a3cb7124d
gerardo.mosca <gerimosca90@gmail.com>
2024-09-17 09:45:08 +0200

    terminado

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,12 @@ const formElement = document.getElementById('form');
 const tasksElement = document.getElementById('tasks');
 const deleteCompletedElement = document.getElementById('delete-completed');
 const itemsLeftElement = document.getElementById('items-left');
+const filtersElement = document.getElementById('filters');
+const allFiltersElement = document.querySelectorAll('.filter');
 
 let allTasks = [];
+
+let filterActive = 'all';
 
 const countItemsLeft = () => {
   if (allTasks.length === 0) {
@@ -20,10 +24,17 @@ const countItemsLeft = () => {
   }
 };
 
+const getFilteredTasks = () => {
+  if (filterActive === 'active') return allTasks.filter((task) => !task.completed);
+  if (filterActive === 'completed') return allTasks.filter((task) => task.completed);
+  return [...allTasks];
+};
+
 const printTasks = () => {
   const fragment = document.createDocumentFragment();
+  const filteredTasks = getFilteredTasks();
 
-  allTasks.forEach((task) => {
+  filteredTasks.forEach((task) => {
     const newTaskContainer = document.createElement('div');
     newTaskContainer.classList.add('task-container');
 
@@ -92,13 +103,23 @@ const deleteAllCompletedTasks = () => {
   printTasks();
 };
 
+const selectFilter = (event) => {
+  const currentFilter = event.target.dataset.filter;
+  if (!currentFilter) return;
+
+  allFiltersElement.forEach((filter) => filter.classList.remove('filter-active'));
+  event.target.classList.add('filter-active');
+  filterActive = currentFilter;
+  printTasks();
+};
+
 countItemsLeft();
 
 deleteCompletedElement.addEventListener('click', deleteAllCompletedTasks);
-
 formElement.addEventListener('submit', (event) => {
   event.preventDefault();
   if (!event.target.task.value.trim()) return;
   createTask(event.target.task.value);
   event.target.reset();
 });
+filtersElement.addEventListener('click', selectFilter);
